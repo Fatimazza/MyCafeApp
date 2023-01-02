649c446282ae4747b6c71d7a6eb1def94501d3ae
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-02 15:48:28 +0700

    Model - Add Model directory containing Cafe Menu and its Data Source

---
+++ app/src/main/java/io/github/fatimazza/mycafeapp/model/FakeMenuDataSource.kt
@@ -0,0 +1,20 @@
+package io.github.fatimazza.mycafeapp.model
+
+import io.github.fatimazza.mycafeapp.R
+
+object FakeMenuDataSource {
+    val dummyMenu = listOf(
+        Menu(1, R.drawable.menu_1, "Japanese Sushi", 35000),
+        Menu(2, R.drawable.menu_2, "Sunny side up Toast", 15000),
+        Menu(3, R.drawable.menu_3, "Veggies Pizza", 65000),
+        Menu(4, R.drawable.menu_4, "Vegetables Salad", 42500),
+        Menu(5, R.drawable.menu_5, "Prawn Ramen with Egg", 65000),
+        Menu(6, R.drawable.menu_6, "Tiramisu Coffee", 17000),
+        Menu(7, R.drawable.menu_7, "Choco Doughnut", 12500),
+        Menu(8, R.drawable.menu_8, "Summer Fruit Salad", 45000),
+        Menu(9, R.drawable.menu_9, "Very Berry Pancake", 32500),
+        Menu(10, R.drawable.menu_10, "Hot Choco", 15500),
+        Menu(11, R.drawable.menu_11, "Summer Tea", 15000),
+        Menu(12, R.drawable.menu_12, "Red Berry Dessert", 13500),
+    )
+}
---
+++ app/src/main/java/io/github/fatimazza/mycafeapp/model/Menu.kt
@@ -0,0 +1,8 @@
+package io.github.fatimazza.mycafeapp.model
+
+data class Menu(
+    val id: Long,
+    val image: Int,
+    val title: String,
+    val price: Int,
+)
