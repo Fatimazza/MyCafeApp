47acaba2998c818d01ee531654bd3e27d30593be
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-23 19:57:12 +0700

    Navigation - Create Navigation Item class under Navigation directory

---
+++ app/src/main/java/io/github/fatimazza/mycafeapp/ui/navigation/NavigationItem.kt
@@ -0,0 +1,9 @@
+package io.github.fatimazza.mycafeapp.ui.navigation
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavigationItem(
+    val title: String,
+    val icon: ImageVector,
+    val screen: Screen
+)
