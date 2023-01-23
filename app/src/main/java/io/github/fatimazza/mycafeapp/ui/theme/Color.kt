067a46d96b56ea59d6310ec426bb7d1c4fa354b6
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-23 19:51:33 +0700

    Theme - Add Colors and Change Theme Colors

--- app/src/main/java/io/github/fatimazza/mycafeapp/ui/theme/Color.kt
+++ app/src/main/java/io/github/fatimazza/mycafeapp/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Green200 = Color(0xFFC8E6C9)
+val Green500 = Color(0xFF97d498)
+val Green700 = Color(0xFF97b498)
+val Purple400 = Color(0xFFBB86FC)
