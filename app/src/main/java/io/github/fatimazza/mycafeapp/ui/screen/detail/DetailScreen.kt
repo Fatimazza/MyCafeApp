d36233d7ddb074cdbe5978f1f58233fba26f8940
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-25 10:53:29 +0700

    Detail - Add Column containing Image and Back Icon

--- app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/detail/DetailScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/detail/DetailScreen.kt
@@ -1,11 +1,20 @@
 package io.github.fatimazza.mycafeapp.ui.screen.detail
 
-import androidx.compose.foundation.background
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,14 +25,44 @@ import io.github.fatimazza.mycafeapp.R
 
 @Composable
 fun DetailScreen() {
-    DetailContent()
+    DetailContent(
+        R.drawable.menu_1
+    )
 }
 
 @Composable
 fun DetailContent(
+    @DrawableRes image: Int,
     modifier: Modifier = Modifier
 ) {
     Column(modifier = modifier) {
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .weight(1f)
+        ) {
+            Box {
+                Image(
+                    painter = painterResource(image),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = modifier.height(400.dp)
+                        .fillMaxWidth()
+                        .border(
+                            BorderStroke(4.dp, MaterialTheme.colors.primary),
+                            RoundedCornerShape(
+                                bottomStart = 20.dp, bottomEnd = 20.dp)
+                        )
+                        .clip(RoundedCornerShape(
+                            bottomStart = 20.dp, bottomEnd = 20.dp))
+                )
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = stringResource(R.string.back),
+                    modifier = Modifier.padding(16.dp).clickable { }
+                )
+            }
+        }
         Spacer(
             modifier = Modifier
                 .fillMaxWidth()
@@ -53,6 +92,8 @@ fun DetailContent(
 @Composable
 fun MyCafeAppPreview() {
     MyCafeAppTheme {
-        DetailContent()
+        DetailContent(
+            R.drawable.menu_1
+        )
     }
 }
