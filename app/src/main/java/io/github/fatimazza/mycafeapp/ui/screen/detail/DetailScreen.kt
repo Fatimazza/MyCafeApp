66d7b8420fbfb3d3f5b50d11de3ecf9e387e2a27
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-25 09:24:13 +0700

    Detail - Add Column containing Item Counter and Order Button

--- app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/detail/DetailScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/detail/DetailScreen.kt
@@ -1,9 +1,18 @@
 package io.github.fatimazza.mycafeapp.ui.screen.detail
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.github.fatimazza.mycafeapp.ui.components.ItemCounter
+import io.github.fatimazza.mycafeapp.ui.components.OrderButton
 import io.github.fatimazza.mycafeapp.ui.theme.MyCafeAppTheme
+import io.github.fatimazza.mycafeapp.R
 
 @Composable
 fun DetailScreen() {
@@ -14,7 +23,30 @@ fun DetailScreen() {
 fun DetailContent(
     modifier: Modifier = Modifier
 ) {
-
+    Column(modifier = modifier) {
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(4.dp)
+                .background(Color.LightGray)
+        )
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ItemCounter(
+                0,
+                1,
+                onItemIncreased = {  },
+                onItemDecreased = {  },
+                modifier = Modifier.align(Alignment.CenterHorizontally).padding(bottom = 16.dp)
+            )
+            OrderButton(
+                text = stringResource(R.string.add_to_cart, 0),
+                enabled = true,
+                onClick = {}
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
