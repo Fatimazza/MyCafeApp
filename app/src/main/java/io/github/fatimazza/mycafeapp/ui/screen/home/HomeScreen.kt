1d4f6e9ecfb9c0c6aeda2c9f1249ab86ac112e1c
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-25 08:14:01 +0700

    Home - Display Grid List of Cafe Menu from Data source

--- app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/home/HomeScreen.kt
@@ -1,20 +1,15 @@
 package io.github.fatimazza.mycafeapp.ui.screen.home
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import io.github.fatimazza.mycafeapp.R
+import io.github.fatimazza.mycafeapp.model.FakeMenuDataSource
 import io.github.fatimazza.mycafeapp.model.Menu
 import io.github.fatimazza.mycafeapp.ui.components.MenuItem
 import io.github.fatimazza.mycafeapp.ui.theme.MyCafeAppTheme
@@ -23,12 +18,10 @@ import io.github.fatimazza.mycafeapp.ui.theme.MyCafeAppTheme
 fun HomeScreen(
     modifier: Modifier = Modifier
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
-    ) {
-        Text(stringResource(R.string.menu_home))
-    }
+    HomeContent(
+        orderMenu = FakeMenuDataSource.dummyMenus,
+        modifier = modifier
+    )
 }
 
 @Composable
