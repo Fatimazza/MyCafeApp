6319153e3353d2de5dbe102f4e0ec3ce2419b0f7
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-25 11:02:05 +0700

    Detail - Add Column containing Title, Description, and Price

--- app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/detail/DetailScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/detail/DetailScreen.kt
@@ -1,11 +1,13 @@
 package io.github.fatimazza.mycafeapp.ui.screen.detail
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
@@ -14,8 +16,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.fatimazza.mycafeapp.ui.components.ItemCounter
@@ -26,15 +31,21 @@ import io.github.fatimazza.mycafeapp.R
 @Composable
 fun DetailScreen() {
     DetailContent(
-        R.drawable.menu_1
+        R.drawable.menu_1,
+        R.string.food_sushi,
+        15000
     )
 }
 
 @Composable
 fun DetailContent(
     @DrawableRes image: Int,
+    @StringRes title: Int,
+    price: Int,
     modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
+
     Column(modifier = modifier) {
         Column(
             modifier = Modifier
@@ -60,6 +71,31 @@ fun DetailContent(
                     imageVector = Icons.Default.ArrowBack,
                     contentDescription = stringResource(R.string.back),
                     modifier = Modifier.padding(16.dp).clickable { }
+                )
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(
+                    text = context.getString(title),
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.h5.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    ),
+                )
+                Text(
+                    text = stringResource(R.string.item_price, price),
+                    style = MaterialTheme.typography.subtitle1.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    ),
+                    color = MaterialTheme.colors.secondary
+                )
+                Text(
+                    text = stringResource(R.string.lorem_ipsum),
+                    style = MaterialTheme.typography.body2,
+                    textAlign = TextAlign.Justify,
                 )
             }
         }
@@ -93,7 +129,9 @@ fun DetailContent(
 fun MyCafeAppPreview() {
     MyCafeAppTheme {
         DetailContent(
-            R.drawable.menu_1
+            R.drawable.menu_1,
+            R.string.food_sushi,
+            15000
         )
     }
 }
