756d155d1a12cf70e5e47d132d39dadaee32d717
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-25 08:08:10 +0700

    Home - Add Composable to display Grid List of Cafe Menu

--- app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/home/HomeScreen.kt
@@ -1,14 +1,22 @@
 package io.github.fatimazza.mycafeapp.ui.screen.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.fatimazza.mycafeapp.R
+import io.github.fatimazza.mycafeapp.model.Menu
+import io.github.fatimazza.mycafeapp.ui.components.MenuItem
 import io.github.fatimazza.mycafeapp.ui.theme.MyCafeAppTheme
 
 @Composable
@@ -20,6 +28,28 @@ fun HomeScreen(
         contentAlignment = Alignment.Center,
     ) {
         Text(stringResource(R.string.menu_home))
+    }
+}
+
+@Composable
+fun HomeContent(
+    orderMenu: List<Menu>,
+    modifier: Modifier = Modifier,
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(160.dp),
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier
+    ) {
+        items(orderMenu) { data ->
+            MenuItem(
+                image = data.image,
+                title = data.title,
+                price = data.price,
+            )
+        }
     }
 }
 
