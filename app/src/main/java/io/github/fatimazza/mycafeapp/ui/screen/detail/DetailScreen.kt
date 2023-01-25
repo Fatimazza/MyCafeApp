b1e4b20e37212ce378fd2221fcec2e907675e786
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-25 16:15:02 +0700

    Detail MVVM - Using View Model to get Menu Detail by Id

--- app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/detail/DetailScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/detail/DetailScreen.kt
@@ -10,15 +10,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -34,6 +30,7 @@ import io.github.fatimazza.mycafeapp.ui.theme.MyCafeAppTheme
 import io.github.fatimazza.mycafeapp.R
 import io.github.fatimazza.mycafeapp.di.Injection
 import io.github.fatimazza.mycafeapp.ui.ViewModelFactory
+import io.github.fatimazza.mycafeapp.ui.common.UiState
 
 @Composable
 fun DetailScreen(
@@ -44,12 +41,23 @@ fun DetailScreen(
     navigateBack: () -> Unit,
     navigateToCart: () -> Unit
 ) {
-    DetailContent(
-        R.drawable.menu_1,
-        R.string.food_sushi,
-        15000,
-        1
-    )
+    viewModel.uiState.collectAsState(initial = UiState.Loading).value.let { uiState ->
+        when (uiState) {
+            is UiState.Loading -> {
+                viewModel.getMenuById(menuId)
+            }
+            is UiState.Success -> {
+                val data = uiState.data
+                DetailContent(
+                    data.menu.image,
+                    data.menu.title,
+                    data.menu.price,
+                    data.count
+                )
+            }
+            is UiState.Error -> {}
+        }
+    }
 }
 
 @Composable
