678e150775f03ad6615e3af222a598125a3657ab
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-25 08:17:57 +0700

    Home MVVM - Add View Model to get all Menus

---
+++ app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/home/HomeViewModel.kt
@@ -0,0 +1,31 @@
+package io.github.fatimazza.mycafeapp.ui.screen.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.github.fatimazza.mycafeapp.data.MenuRepository
+import io.github.fatimazza.mycafeapp.model.OrderMenu
+import io.github.fatimazza.mycafeapp.ui.common.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+
+class HomeViewModel(
+    private val repository: MenuRepository
+) : ViewModel() {
+    private val _uiState: MutableStateFlow<UiState<List<OrderMenu>>> = MutableStateFlow(UiState.Loading)
+    val uiState: StateFlow<UiState<List<OrderMenu>>>
+        get() = _uiState
+
+    fun getAllMenus() {
+        viewModelScope.launch {
+            repository.getAllMenus()
+                .catch {
+                    _uiState.value = UiState.Error(it.message.toString())
+                }
+                .collect { orderMenus ->
+                    _uiState.value = UiState.Success(orderMenus)
+                }
+        }
+    }
+}
