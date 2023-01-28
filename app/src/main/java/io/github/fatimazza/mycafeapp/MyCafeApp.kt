ba97058729ef13bbddee7371699976a2e5619f70
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-28 16:45:18 +0700

    Navigation - Navigate to other App when clicking Share Order

--- app/src/main/java/io/github/fatimazza/mycafeapp/MyCafeApp.kt
+++ app/src/main/java/io/github/fatimazza/mycafeapp/MyCafeApp.kt
@@ -1,5 +1,7 @@
 package io.github.fatimazza.mycafeapp
 
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -9,6 +11,7 @@ import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -56,9 +59,10 @@ fun MyCafeApp(
                 )
             }
             composable(Screen.Cart.route) {
+                val context = LocalContext.current
                 CartScreen(
                     onOrderButtonClicked = { message ->
-
+                        shareOrder(context, message)
                     }
                 )
             }
@@ -89,6 +93,21 @@ fun MyCafeApp(
             }
         }
     }
+}
+
+private fun shareOrder(context: Context, summary: String) {
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_SUBJECT, context.getString(R.string.share_subject))
+        putExtra(Intent.EXTRA_TEXT, summary)
+    }
+
+    context.startActivity(
+        Intent.createChooser(
+            intent,
+            context.getString(R.string.share_subject)
+        )
+    )
 }
 
 @Composable
