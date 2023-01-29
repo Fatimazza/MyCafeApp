a32ae750f1b5597569d59a71eb02a423b44a6a24
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-29 13:12:06 +0700

    Home - Add Search bar Composable UI

--- app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/home/HomeScreen.kt
@@ -1,17 +1,25 @@
 package io.github.fatimazza.mycafeapp.ui.screen.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import io.github.fatimazza.mycafeapp.R
 import io.github.fatimazza.mycafeapp.di.Injection
 import io.github.fatimazza.mycafeapp.model.OrderMenu
 import io.github.fatimazza.mycafeapp.ui.ViewModelFactory
@@ -50,24 +58,63 @@ fun HomeContent(
     modifier: Modifier = Modifier,
     navigateToDetail: (Long) -> Unit
 ) {
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(160.dp),
-        contentPadding = PaddingValues(16.dp),
-        horizontalArrangement = Arrangement.spacedBy(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        modifier = modifier
-    ) {
-        items(orderMenu) { data ->
-            MenuItem(
-                image = data.menu.image,
-                title = data.menu.title,
-                price = data.menu.price,
-                modifier = Modifier.clickable {
-                    navigateToDetail(data.menu.id)
-                }
-            )
+    Column() {
+        SearchBar(
+            query = "",
+            onQueryChange = {},
+            modifier = modifier.background(MaterialTheme.colors.primary)
+        )
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(160.dp),
+            contentPadding = PaddingValues(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = modifier
+        ) {
+            items(orderMenu) { data ->
+                MenuItem(
+                    image = data.menu.image,
+                    title = data.menu.title,
+                    price = data.menu.price,
+                    modifier = Modifier.clickable {
+                        navigateToDetail(data.menu.id)
+                    }
+                )
+            }
         }
     }
+}
+
+@Composable
+fun SearchBar(
+    query: String,
+    onQueryChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = query,
+        onValueChange = onQueryChange,
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface,
+            disabledIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ),
+        placeholder = {
+            Text(stringResource(R.string.search_menu))
+        },
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .heightIn(min = 48.dp)
+            .clip(RoundedCornerShape(16.dp))
+    )
 }
 
 @Preview(showBackground = true)
