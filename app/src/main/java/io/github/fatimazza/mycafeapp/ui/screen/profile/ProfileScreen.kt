335cc013c6f39c0c5d596ca8a03a739ca8e06994
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-30 11:26:40 +0700

    Profile - Add Image and Texts

--- app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/profile/ProfileScreen.kt
@@ -1,17 +1,24 @@
 package io.github.fatimazza.mycafeapp.ui.screen.profile
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,6 +29,8 @@ import io.github.fatimazza.mycafeapp.ui.theme.MyCafeAppTheme
 fun ProfileScreen(
     modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
+
     Column(
         modifier = modifier.fillMaxSize()
     ) {
@@ -34,6 +43,67 @@ fun ProfileScreen(
                 fontWeight = FontWeight.Bold,
                 fontSize = 18.sp,
                 textAlign = TextAlign.Center
+            )
+        }
+        Column(
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(
+                modifier = modifier.height(32.dp)
+            )
+            Text(
+                text = context.getString(R.string.app_name),
+                textAlign = TextAlign.Center,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.Light
+                ),
+                modifier = modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = context.getString(R.string.developer_desc),
+                textAlign = TextAlign.Center,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.h5.copy(
+                    fontWeight = FontWeight.Light
+                ),
+                modifier = modifier.align(Alignment.CenterHorizontally)
+            )
+            Image(
+                painter = painterResource(R.drawable.menu_1),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .border(
+                        BorderStroke(4.dp, MaterialTheme.colors.primary),
+                        CircleShape
+                    )
+                    .clip(CircleShape)
+            )
+            Text(
+                text = context.getString(R.string.developer_name),
+                textAlign = TextAlign.Center,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.h5.copy(
+                    fontWeight = FontWeight.Light
+                ),
+                modifier = modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = context.getString(R.string.developer_email),
+                textAlign = TextAlign.Center,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.h5.copy(
+                    fontWeight = FontWeight.Light
+                ),
+                modifier = modifier.align(Alignment.CenterHorizontally)
             )
         }
     }
