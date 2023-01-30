511d268224ffc0460d6e85bb88f4c626506ea218
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-30 11:09:50 +0700

    Profile - Add Top App Bar

--- app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/io/github/fatimazza/mycafeapp/ui/screen/profile/ProfileScreen.kt
@@ -1,15 +1,42 @@
 package io.github.fatimazza.mycafeapp.ui.screen.profile
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import io.github.fatimazza.mycafeapp.R
 import io.github.fatimazza.mycafeapp.ui.theme.MyCafeAppTheme
 
 @Composable
 fun ProfileScreen(
     modifier: Modifier = Modifier
 ) {
-    
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        TopAppBar(backgroundColor = MaterialTheme.colors.surface) {
+            Text(
+                text = stringResource(R.string.menu_profile),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp),
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
