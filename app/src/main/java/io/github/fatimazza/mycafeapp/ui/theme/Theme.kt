a343df78c44e9218039d52732601a2683425f436
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-30 11:39:24 +0700

    Theme - Add Accompanist Dependency and its implementation

--- app/src/main/java/io/github/fatimazza/mycafeapp/ui/theme/Theme.kt
+++ app/src/main/java/io/github/fatimazza/mycafeapp/ui/theme/Theme.kt
@@ -5,6 +5,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Green200,
@@ -40,5 +42,13 @@ fun MyCafeAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composa
         typography = Typography,
         shapes = Shapes,
         content = content
+    )
+
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setSystemBarsColor(
+        color = if (darkTheme) Color.Black else Color.White
+    )
+    systemUiController.setStatusBarColor(
+        color = if (darkTheme) Color.Black else Purple400
     )
 }
